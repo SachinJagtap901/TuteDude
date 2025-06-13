5b113cf16cfc8d134558d252c2f6936cc81f48fe
SachinJagtap901 <87950779+SachinJagtap901@users.noreply.github.com>
2025-06-13 16:35:25 +0530

    Add files via upload
    Basic Maths operation by taking Input
    Addition
    Subtraction
    Multiplication
    Division

---
+++ Task1.py
@@ -0,0 +1,12 @@
+n1 = input("enter first number: ")
+n2 = input("enter second number: ")
+n1 = int(n1)
+n2 = int(n2)
+sum = n1 + n2
+sub = n1 - n2
+mult = n1 * n2
+div = n1 / n2
+print("Addition : ",sum)
+print("Subtraction : ",sub)
+print("Multiplication : ",mult)
+print("Division : ",div)
