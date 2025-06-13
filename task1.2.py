2fed88eabb764031967830d6151bb561903f665f
SachinJagtap901 <87950779+SachinJagtap901@users.noreply.github.com>
2025-06-13 16:51:29 +0530

    Add files via upload
    First Name and last name taken as input and executed welcome message with full name

---
+++ task1.2.py
@@ -0,0 +1,6 @@
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+first_name = str(first_name)
+last_name = str(last_name)
+full_name = str(first_name) +" "+ str( last_name)+"!"
+print("Hello", full_name,"Welcome to the python program.")
