b4b0a48b66c061aa2b7a175ac2757945f65bc906
SachinJagtap901 <87950779+SachinJagtap901@users.noreply.github.com>
2025-06-27 14:59:50 +0530

    list slicing and list reversals printing

---
+++ task 5.2.py
@@ -0,0 +1,7 @@
+numbers = list(range(1, 11))  # Create a list from 1 to 10
+first_five = numbers[:5]  # Extract the first five elements
+reversed_five = first_five[::-1] # Reverse the extracted elements
+
+print("Original list:", numbers)
+print("First five elements:", first_five)
+print("Reversed first five elements:", reversed_five)
